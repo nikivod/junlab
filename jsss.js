1ef3292d606f7df0a564ba092cc7c7d4a9378f60
nikivod <58002053+nikivod@users.noreply.github.com>
2019-12-23 00:15:44 +0300

    Add files via upload

---
+++ jsss.js
@@ -0,0 +1,10 @@
+alert ("Now... Say my name.");
+console.log ("Bitch")
+
+let brbad = prompt('Now... Say my name', '');
+
+if (brbad == Heisenberg) {
+  alert( 'you goddamn right!' );
+} else {
+  alert( 'shot' ); // любое значение, кроме 2015
+}
